133ec771222e42ad4499060bf85f002a4bc05b28
William_Twomey <48021997+Reilig35@users.noreply.github.com>
2021-07-28 19:31:25 +0100

    Add project files.

---
+++ VariablesAndDataTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
